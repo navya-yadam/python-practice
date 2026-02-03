7b653db98b05c58185262ade276886e464a82584
navya yadam <navyasrinavya39@gmail.com>
2026-02-03 19:49:40 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,20 @@
+name="Abhishek"
+# colors={"orange", "pink","green"}
+# for color in colors:
+    # print(color)
+    #   for i in color:
+        #    print(i)
+
+# for k in range(5):
+    # print(k)
+
+# for k in range(5):
+    # print(k+1)
+
+for k in range(1, 12, 3):
+    print(k)
+      
+# for k in range(1, 2000):
+    # print(k)
+        
+    
